5c68ff618891f3463fdb6179fb38e56db6a47a2d
Koppolu Koushik <122113703+Koushik00-tech@users.noreply.github.com>
2023-10-05 19:11:32 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,46 @@
+let nameEl = document.getElementById("name");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+
+let emailEl = document.getElementById("email");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+
+let workingStatusEl = document.getElementById("status");
+let genderMaleEl = document.getElementById("genderMale");
+let genderFemaleEl = document.getElementById("genderFemale");
+
+let addUserFormElement = document.getElementById("addUserForm");
+
+let formData = {
+    name: "",
+    email: "",
+    status: "Active",
+    gender: "Male"
+};
+
+nameEl.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        nameErrMsgEl.textContent = "Required*";
+    } else {
+        nameErrMsgEl.textContent = "";
+    }
+
+    formData.name = event.target.value;
+});
+
+emailEl.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        emailErrMsgEl.textContent = "Required*";
+    } else {
+        emailErrMsgEl.textContent = "";
+    }
+
+    formData.email = event.target.value;
+});
+
+workingStatusEl.addEventListener("change", function(event) {
+    formData.status = event.target.value;
+});
+
+genderMaleEl.addEventListener("change", function(event) {
+    formData.gender = event.target.value;
+});
